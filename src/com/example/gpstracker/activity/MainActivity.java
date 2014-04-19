3fe00887a00d7432ce1bdbd48721e7f74dfe2a2e
acer <acer@acer-PC>
2014-04-19 16:51:06 +0100

    Implement GpsTrackerService

---
+++ src/com/example/gpstracker/activity/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.gpstracker.activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.gpstracker.R;
+import com.example.gpstracker.service.GpsTrackerService;
+
+public class MainActivity extends Activity implements OnClickListener {
+	private static final String TAG = MainActivity.class.getSimpleName();
+
+	private Button registerReceiver;
+	private Button unregisterReceiver;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Log.e(TAG, "onCreate");
+
+		registerReceiver = (Button) findViewById(R.id.registerReceiver);
+		unregisterReceiver = (Button) findViewById(R.id.unregisterReceiver);
+
+		registerReceiver.setOnClickListener(this);
+		unregisterReceiver.setOnClickListener(this);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+
+	/**
+	 * Show GPS disabled alert
+	 */
+	private void showGPSDisabledAlert() {
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+		alertDialogBuilder
+				.setMessage("GPS is disabled in your device. Would you like to enable it?");
+		alertDialogBuilder.setCancelable(false);
+
+		alertDialogBuilder.setPositiveButton(
+				"Goto Settings Page To Enable GPS",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						Intent callGPSSettingIntent = new Intent(
+								android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+						startActivity(callGPSSettingIntent);
+					}
+				});
+
+		alertDialogBuilder.setNegativeButton("Cancel",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+
+		AlertDialog alert = alertDialogBuilder.create();
+		alert.show();
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.registerReceiver:
+
+			LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+			if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+				Toast.makeText(getApplicationContext(), "GPS is Enabled",
+						Toast.LENGTH_SHORT).show();
+
+				// Start service
+				Intent intent = new Intent(getApplicationContext(), GpsTrackerService.class);
+				startService(intent);
+
+			} else {
+				showGPSDisabledAlert();
+			}
+			break;
+		case R.id.unregisterReceiver:
+			// Stop service
+			Intent intent = new Intent(getApplicationContext(), GpsTrackerService.class);
+			stopService(intent);
+			break;
+		default:
+			break;
+		}
+	}
+}
--- src/com/example/gpstracker/service/GpsTrackerService.java
+++ src/com/example/gpstracker/service/GpsTrackerService.java
@@ -1,31 +1,25 @@
-package com.example.gpstracker;
+package com.example.gpstracker.service;
 
-import android.app.Activity;
-import android.app.AlertDialog;
+import android.app.Service;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener {
-	private static final String TAG = MainActivity.class.getSimpleName();
+public class GpsTrackerService extends Service {
+	private static final String TAG = GpsTrackerService.class.getSimpleName();
+	private static final int UPDATE_PERIODE = 1000 * 60 * 15;
 
 	private LocationManager locationManager;
 	private Location currentLocation;
 	private boolean listening;
 	private boolean gpsInUse;
-
-	private Button registerReceiver;
-	private Button unregisterReceiver;
 
 	/**
 	 * Defines a listener that responds to location updates
@@ -71,18 +65,35 @@ public class MainActivity extends Activity implements OnClickListener {
 	};
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Log.e(TAG, "onCreate");
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
 
 		this.locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	}
 
-		registerReceiver = (Button) findViewById(R.id.registerReceiver);
-		unregisterReceiver = (Button) findViewById(R.id.unregisterReceiver);
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
 
-		registerReceiver.setOnClickListener(this);
-		unregisterReceiver.setOnClickListener(this);
+		this.requestLastKnownLocation();
+
+		if (currentLocation != null) {
+			String msg = String.format("LastKnownLocation: lat=%s,  lng=%s",
+					currentLocation.getLatitude(),
+					currentLocation.getLongitude());
+
+			Log.d(TAG, msg);
+			Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT)
+					.show();
+		}
+
+		this.startLocationUpdates();
+
+		return START_STICKY;
 	}
 
 	@Override
@@ -90,7 +101,6 @@ public class MainActivity extends Activity implements OnClickListener {
 		stopLocationUpdates();
 
 		this.locationManager = null;
-
 		super.onDestroy();
 	}
 
@@ -122,8 +132,8 @@ public class MainActivity extends Activity implements OnClickListener {
 	private void startLocationUpdates() {
 		listening = false;
 
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
-				0, locationListener);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				UPDATE_PERIODE, 0, locationListener);
 
 		// setting gpsInUse to true, but listening is still false at this point
 		// listening is set to true with first location update in
@@ -138,69 +148,5 @@ public class MainActivity extends Activity implements OnClickListener {
 		locationManager.removeUpdates(locationListener);
 		listening = false;
 		gpsInUse = false;
-	}
-
-	/**
-	 * Show GPS disabled alert
-	 */
-	private void showGPSDisabledAlert() {
-		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-		alertDialogBuilder.setMessage("GPS is disabled in your device. Would you like to enable it?");
-		alertDialogBuilder.setCancelable(false);
-		
-		alertDialogBuilder.setPositiveButton("Goto Settings Page To Enable GPS",
-						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int id) {
-								Intent callGPSSettingIntent = new Intent(
-										android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-								startActivity(callGPSSettingIntent);
-							}
-						});
-		
-		alertDialogBuilder.setNegativeButton("Cancel",
-				new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						dialog.cancel();
-					}
-				});
-		
-		AlertDialog alert = alertDialogBuilder.create();
-		alert.show();
-	}
-
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.registerReceiver:
-			
-			if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-				Toast.makeText(getApplicationContext(), "GPS is Enabled", Toast.LENGTH_SHORT)
-						.show();
-
-				this.requestLastKnownLocation();
-
-				if (currentLocation != null) {
-					String msg = String.format(
-							"LastKnownLocation: lat=%s,  lng=%s",
-							currentLocation.getLatitude(),
-							currentLocation.getLongitude());
-
-					Log.d(TAG, msg);
-					Toast.makeText(getApplicationContext(), msg,
-							Toast.LENGTH_SHORT).show();
-				}
-
-				this.startLocationUpdates();
-
-			} else {
-				showGPSDisabledAlert();
-			}
-			break;
-		case R.id.unregisterReceiver:
-			this.stopLocationUpdates();
-			break;
-		default:
-			break;
-		}
 	}
 }
